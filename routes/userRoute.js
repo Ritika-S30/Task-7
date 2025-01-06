28f8150967227298f1a3c9d2864d89b2255557fb
Ritika-S30 <ritikas.22eie@kongu.edu>
2025-01-06 15:51:50 +0530

    new5

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import express from 'express';
 import { fetch,create,update ,deleteUser} from '../controller/userController.js';
 
@@ -8,7 +8,9 @@ router.post('/create', create);
 router.put('/update/:id', update);
 router.delete('/delete/:id', deleteUser);
 export default router; 
-=======
+
+
+
 // import express from 'express';
 // import {fetch,create,update} from '../controller/userController.js'
 // const router =express.Router();
@@ -21,12 +23,4 @@ export default router;
 // export default router;
 
 
-import express from 'express';
-import { fetch,create,update } from '../controller/userController.js';
-const router = express.Router();
-router.get('/fetch',fetch);
-router.post('/create',create);
-router.put('/update/:id',update);
 
-export default router;
->>>>>>> bb71f4d27c78c1c09334c25a0e8d8858adaf239d
